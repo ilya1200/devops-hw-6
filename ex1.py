3dd78afbd07615a307e08c93d046da7192793179
ilya <ilya1200@gmail.com>
2021-12-14 21:14:14 +0200

    add task4

--- ex1.py
+++ ex1.py
@@ -5,17 +5,20 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.remote.webelement import WebElement
 
 CHROME_DRIVER = "/home/ilya/PycharmProjects/devops-hw-6/chromedriver"
 FIREFOX_DRIVER = "/usr/bin/geckodriver"
 
 urls: dict = {
     "walla": "http://www.walla.co.il",
-    "ynet": "http://ynet.co.il"
+    "ynet": "http://ynet.co.il",
+    "google_translate": "https://translate.google.com/"
 }
 
 locators = {
-    "walla_title": (By.CSS_SELECTOR, '[title="וואלה! NEWS"]')
+    "walla_title": (By.CSS_SELECTOR, '[title="וואלה! NEWS"]'),
+    "google_translate_source": (By.CSS_SELECTOR, "textarea[aria-label='Source text']"),
 }
 
 
@@ -26,7 +29,7 @@ def chrome_driver() -> WebDriver:
     chrome_options.add_argument("--disable-extensions")
     chrome_options.add_argument("--headless")
     os.chmod(CHROME_DRIVER, 0o755)
-    chrome_driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=CHROME_DRIVER)
+    chrome_driver: WebDriver = webdriver.Chrome(chrome_options=chrome_options, executable_path=CHROME_DRIVER)
     return chrome_driver
 
 
@@ -35,9 +38,9 @@ def firefox_driver() -> WebDriver:
     firefox_options = Options()
     firefox_options.add_argument("--headless")
     os.chmod(CHROME_DRIVER, 0o755)
-    firefox_options = webdriver.Firefox(options=firefox_options, executable_path=FIREFOX_DRIVER)
+    firefox_driver: WebDriver = webdriver.Firefox(options=firefox_options, executable_path=FIREFOX_DRIVER)
 
-    return firefox_options
+    return firefox_driver
 
 
 def task_1():
@@ -82,6 +85,17 @@ def task_3():
     assert firefox_page_title == chrome_page_title
 
 
+def task_4():
+    chrome: WebDriver = chrome_driver()
+    chrome.get(urls['google_translate'])
+    print('Page title: ', chrome.title)
+
+    source_text_element: WebElement = chrome.find_element(locators["google_translate_source"])
+    source_text_element.send_keys("תפוח")
+    chrome.quit()
+
+
 task_1()
 task_2()
 task_3()
+task_4()
